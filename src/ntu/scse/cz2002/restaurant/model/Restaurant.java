96117abe3603c82d77194b2a2ed171e120a4123f
Hans Tjua <hanstjua@yahoo.co.id>
2019-04-17 15:09:59 +0800

    removed menu attribute

--- src/ntu/scse/cz2002/restaurant/model/Restaurant.java
+++ src/ntu/scse/cz2002/restaurant/model/Restaurant.java
@@ -7,9 +7,6 @@ public class Restaurant {
 	private int noOfTable = 30;
 	private Staff currentStaff;
   private Menu menu;
-  private String itemsFilename = "items.dat";
-  private String promosFilename = "promos.dat";
-
 
 	public Restaurant() {
 	}
@@ -17,23 +14,14 @@ public class Restaurant {
 	public Restaurant(int noOfTable, Staff currentStaff) {
 		this.noOfTable = noOfTable;
 		this.currentStaff = currentStaff;
-
-    createMenu(this.itemsFilename, this.promosFilename);
 	}
-
-  private void createMenu(String itemsFilename, String promosFilename){
-    ArrayList<MenuItem> items = (ArrayList<MenuItem>)DataAccessor.read(itemsFilename);
-    ArrayList<Promotion> promotions = (ArrayList<Promotion>)DataAccessor.read(promosFilename);
-
-    this.menu = new Menu(items, promotions);
-  }
 
 	public int getNoOfTable() { return noOfTable; }
 
 	public Staff getCurrentStaff() { return currentStaff; }
 
 	public void setCurrentStaff(Staff currentStaff) { this.currentStaff = currentStaff; };
-	
+
 	public void initialize(String filename_Staff, String filename_, String filename) {
 
 	}
