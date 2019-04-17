81e5f06b41d5f1d837313c4514f2773273666c73
Hans Tjua <hanstjua@yahoo.co.id>
2019-04-17 12:45:52 +0800

    fixed conflicts

--- src/ntu/scse/cz2002/restaurant/control/MenuController.java
+++ src/ntu/scse/cz2002/restaurant/control/MenuController.java
@@ -8,149 +8,166 @@ import ntu.scse.cz2002.restaurant.model.Promotion;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class MenuController {
-	private Menu menu;
-	private ArrayList<String> types;
+public class MenuController{
+    private Menu menu;
+    private ArrayList<String> types;
 
-	public MenuController() {
-		String itemFilename = "items.dat";
-		String promoFilename = "promos.dat";
+    public MenuController(ArrayList<MenuItem> items, ArrayList<Promotion> promotions){
+        // String itemFilename = "items.dat";
+        // String promoFilename = "promos.dat";
 
-		try {
-			this.loadItems(itemFilename, promoFilename);
-		} catch (Exception e) {
-			System.out.println("No menu data found.");
-		}
-	}
+        // try{
+        //     this.loadItems(itemFilename, promoFilename);
+        // } catch (Exception e){System.out.println("No menu data found.");}
 
-	private void loadItems(String itemFilename, String promoFilename) {
-		ArrayList<MenuItem> items = (ArrayList<MenuItem>) MenuDA.read(itemFilename);
-		ArrayList<Promotion> promotions = (ArrayList<Promotion>) MenuDA.read(promoFilename);
+        if(items != null || promotions != null){
+            this.menu = new Menu(items, promotions);
 
-		if (items != null || promotions != null) {
-			this.menu = new Menu(items, promotions);
+            System.out.println("Menu contents successfully loaded!");
+            updateTypesList(items);
+        }
+        else{
+            this.menu = new Menu();
 
-			System.out.println("Menu contents successfully loaded!");
-			updateTypesList(items);
-		} else {
-			this.menu = new Menu();
+            System.out.println("Menu is empty");
+        }
+    }
 
-			System.out.println("Menu is empty");
-		}
-	}
+    // deprecated
+    private void loadItems(String itemFilename, String promoFilename){
+        ArrayList<MenuItem> items = (ArrayList<MenuItem>)MenuDA.read(itemFilename);
+        ArrayList<Promotion> promotions = (ArrayList<Promotion>)MenuDA.read(promoFilename);
 
-	private void updateTypesList(ArrayList<MenuItem> items) {
-		for (int i = 0; i < items.size(); i++) {
-			if (this.types.contains(items.get(i).getType())) {
-				continue;
-			} else {
-				this.types.add(items.get(i).getType());
-			}
-		}
+        if(items != null || promotions != null){
+            this.menu = new Menu(items, promotions);
 
-		System.out.println("Types list successfully updated!");
-	}
+            System.out.println("Menu contents successfully loaded!");
+            updateTypesList(items);
+        }
+        else{
+            this.menu = new Menu();
 
-	public Menu getMenu() {
-		return menu;
-	}
+            System.out.println("Menu is empty");
+        }
+    }
 
-	public void addItem(MenuItem item) {
-		this.menu.addMenuItem(item);
-		System.out.println("The following menu item has been added to the menu:");
-		System.out.println("  Name: " + item.getName());
-		System.out.println("  Description: " + item.getDescription());
-		System.out.println("  Price: $" + String.valueOf(item.getPrice()));
-		System.out.println("  Type: " + item.getType());
-	}
+    private void updateTypesList(ArrayList<MenuItem> items){
+        for(int i=0;i<items.size();i++){
+            if(this.types.contains(items.get(i).getType())){
+                continue;
+            }
+            else{
+                this.types.add(items.get(i).getType());
+            }
+        }
 
-	public void addItem(Promotion promotion) {
-		this.menu.addPromotion(promotion);
-		System.out.println("The following set has been addded to the menu:");
-		System.out.println("  Name: " + promotion.getPromotionName());
-		System.out.println("  Description: " + promotion.getPromotionDescription());
-		System.out.println("  Price: $" + String.valueOf(promotion.getPrice()));
-	}
+        System.out.println("Types list successfully updated!");
+    }
 
-	public void removeItem(String name) {
-		if (this.menu.removeMenuItem(name) == 0) {
-			if (this.menu.removePromotion(name) == 0) {
-				System.out.println(name + " does not exist in menu!");
-				return;
-			}
-		}
+    public Menu getMenu() {
+    	return menu;
+    }
+    
+    public void addItem(MenuItem item){
+        this.menu.addMenuItem(item);
+        System.out.println("The following menu item has been added to the menu:");
+        System.out.println("  Name: " + item.getName());
+        System.out.println("  Description: " + item.getDescription());
+        System.out.println("  Price: $" +  String.valueOf(item.getPrice()));
+        System.out.println("  Type: " + item.getType());
+    }
 
-		System.out.println(name + " has been successfully deleted from menu!");
-	}
+    public void addItem(Promotion promotion){
+        this.menu.addPromotion(promotion);
+        System.out.println("The following set has been addded to the menu:");
+        System.out.println("  Name: " + promotion.getPromotionName());
+        System.out.println("  Description: " + promotion.getPromotionDescription());
+        System.out.println("  Price: $" + String.valueOf(promotion.getPrice()));
+    }
 
-	public void updateItem(String name, String description, int price) {
-		MenuItem item;
-		Promotion promotion;
+    public void removeItem(String name){
+        if(this.menu.removeMenuItem(name) == 0){
+            if(this.menu.removePromotion(name) == 0){
+                System.out.println(name + " does not exist in menu!");
+                return;
+            }
+        }
 
-		if ((item = this.menu.getMenuItem(name)) != null) {
-			item.setDescription(description);
-			item.setPrice(price);
-			System.out.println("Item susccessfully updated!");
-		} else if ((promotion = this.menu.getPromotion(name)) != null) {
-			promotion.setPromotionDescription(description);
-			promotion.setPrice(price);
-			System.out.println("Item susccessfully updated!");
-		} else {
-			System.out.println(name + " does not exist in menu!");
-		}
-	}
+        System.out.println(name + " has been successfully deleted from menu!");
+    }
 
-	public void printItemsByCategory() {
-		for (int i = 0; i < this.types.size(); i++) {
-			System.out.println(this.types.get(i));
-			for (int j = 0; j < this.menu.getItemCount(); j++) {
-				MenuItem item = this.menu.getItemList().get(j);
+    public void updateItem(String name, String description, int price){
+        MenuItem item;
+        Promotion promotion;
 
-				if (item.getType().equals(this.types.get(i))) {
-					System.out.println(item.getName() + "  $" + item.getPrice());
-					System.out.println(item.getDescription());
-				}
-			}
-		}
+        if((item = this.menu.getMenuItem(name)) != null){
+            item.setDescription(description);
+            item.setPrice(price);
+            System.out.println("Item susccessfully updated!");
+        }
+        else if((promotion = this.menu.getPromotion(name)) != null){
+            promotion.setPromotionDescription(description);
+            promotion.setPrice(price);
+            System.out.println("Item susccessfully updated!");
+        }
+        else{
+            System.out.println(name + " does not exist in menu!");
+        }
+    }
 
-		System.out.printf("\nPromotion\n");
+    public void printItemsByCategory(){
+        for(int i=0;i<this.types.size();i++){
+            System.out.println(this.types.get(i));
+            for(int j=0;j<this.menu.getItemCount();j++){
+                MenuItem item = this.menu.getItemList().get(j);
 
-		for (int k = 0; k < this.menu.getPromotionCount(); k++) {
-			Promotion promotion = this.menu.getPromotionList().get(k);
-			System.out.println(promotion.getPromotionName() + " $" + String.valueOf(promotion.getPrice()));
-			System.out.println("  " + promotion.getPromotionDescription());
-		}
-	}
+                if(item.getType().equals(this.types.get(i))){
+                    System.out.println(item.getName() + "  $" + item.getPrice());
+                    System.out.println(item.getDescription());
+                }
+            }
+        }
 
-	public void printItemsByName() {
-		ArrayList<String> names = new ArrayList<String>();
-		MenuItem item;
-		Promotion promotion;
+        System.out.printf("\nPromotion\n");
 
-		for (int i = 0; i < this.menu.getItemCount(); i++) {
-			names.add(this.menu.getItemList().get(i).getName());
-		}
+        for(int k=0;k<this.menu.getPromotionCount();k++){
+            Promotion promotion = this.menu.getPromotionList().get(k);
+            System.out.println(promotion.getPromotionName() + " $" + String.valueOf(promotion.getPrice()));
+            System.out.println("  " + promotion.getPromotionDescription());
+        }
+    }
 
-		for (int j = 0; j < this.menu.getPromotionCount(); j++) {
-			names.add(this.menu.getPromotionList().get(j).getPromotionName());
-		}
+    public void printItemsByName(){
+        ArrayList<String> names = new ArrayList<String>();
+        MenuItem item;
+        Promotion promotion;
 
-		Collections.sort(names);
+        for(int i=0;i<this.menu.getItemCount();i++){
+            names.add(this.menu.getItemList().get(i).getName());
+        }
 
-		for (int k = 0; k < names.size(); k++) {
-			if (this.menu.getMenuItem(names.get(k)) == null) {
-				if (this.menu.getPromotion(names.get(k)) == null) {
-					System.out.println(names.get(k) + " does not exist in menu!");
-				} else {
-					promotion = this.menu.getPromotion(names.get(k));
-					System.out.println(names.get(k) + " $" + String.valueOf(promotion.getPrice()));
-					System.out.println("  " + promotion.getPromotionDescription());
-				}
-			} else {
-				item = this.menu.getMenuItem(names.get(k));
-				System.out.println(names.get(k) + " $" + String.valueOf(item.getPrice()));
-				System.out.println("  " + item.getDescription());
-			}
-		}
-	}
+        for(int j=0;j<this.menu.getPromotionCount();j++){
+            names.add(this.menu.getPromotionList().get(j).getPromotionName());
+        }
+
+        Collections.sort(names);
+
+        for(int k=0;k<names.size();k++){
+            if(this.menu.getMenuItem(names.get(k)) == null){
+                if(this.menu.getPromotion(names.get(k)) == null){
+                    System.out.println(names.get(k) + " does not exist in menu!");
+                }
+                else{
+                    promotion = this.menu.getPromotion(names.get(k));
+                    System.out.println(names.get(k) + " $" + String.valueOf(promotion.getPrice()));
+                    System.out.println("  " + promotion.getPromotionDescription());
+                }
+            }
+            else{
+                item = this.menu.getMenuItem(names.get(k));
+                System.out.println(names.get(k) + " $" + String.valueOf(item.getPrice()));
+                System.out.println("  " + item.getDescription());
+            }
+        }
+    }
 }
--- src/ntu/scse/cz2002/restaurant/model/Restaurant.java
+++ src/ntu/scse/cz2002/restaurant/model/Restaurant.java
@@ -1,8 +1,15 @@
 package ntu.scse.cz2002.restaurant.model;
 
+import ntu.scse.cz2002.restaurant.dataAccess.DataAccessor;
+import java.util.ArrayList;
+
 public class Restaurant {
 	private int noOfTable = 30;
 	private Staff currentStaff;
+  private Menu menu;
+  private String itemsFilename = "items.dat";
+  private String promosFilename = "promos.dat";
+
 
 	public Restaurant() {
 	}
@@ -10,22 +17,24 @@ public class Restaurant {
 	public Restaurant(int noOfTable, Staff currentStaff) {
 		this.noOfTable = noOfTable;
 		this.currentStaff = currentStaff;
+
+    createMenu(this.itemsFilename, this.promosFilename);
 	}
 
-	public int getNoOfTable() {
-		return noOfTable;
-	}
+  private void createMenu(String itemsFilename, String promosFilename){
+    ArrayList<MenuItem> items = (ArrayList<MenuItem>)DataAccessor.read(itemsFilename);
+    ArrayList<Promotion> promotions = (ArrayList<Promotion>)DataAccessor.read(promosFilename);
 
-	public Staff getCurrentStaff() {
-		return currentStaff;
-	}
+    this.menu = new Menu(items, promotions);
+  }
 
-	public void setCurrentStaff(Staff currentStaff) {
-		this.currentStaff = currentStaff;
-	};
+	public int getNoOfTable() { return noOfTable; }
 
+	public Staff getCurrentStaff() { return currentStaff; }
+
+	public void setCurrentStaff(Staff currentStaff) { this.currentStaff = currentStaff; };
+	
 	public void initialize(String filename_Staff, String filename_, String filename) {
 
 	}
-
 }
