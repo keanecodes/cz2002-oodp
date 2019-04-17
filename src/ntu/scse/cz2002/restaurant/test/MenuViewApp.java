b776a66b384f2af4aa513615c9db7d977ffb26cc
Hans Tjua <hanstjua@yahoo.co.id>
2019-04-17 23:43:13 +0800

    created test app folder

---
+++ src/ntu/scse/cz2002/restaurant/test/MenuViewApp.java
@@ -0,0 +1,9 @@
+package ntu.scse.cz2002.restaurant.test;
+
+import ntu.scse.cz2002.restaurant.view.MenuView;
+
+public class MenuViewApp{
+    public static void main(String args[]){
+        MenuView.MenuUI();
+    }
+}
