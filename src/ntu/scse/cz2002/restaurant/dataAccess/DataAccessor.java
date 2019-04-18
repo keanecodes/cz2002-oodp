c317f281a283d702bf2ea8e9ffc4b680a8bce825
Keane <keanecodes@gmail.com>
2019-04-18 15:50:05 +0800

    Change save data location

--- src/ntu/scse/cz2002/restaurant/dataAccess/DataAccessor.java
+++ src/ntu/scse/cz2002/restaurant/dataAccess/DataAccessor.java
@@ -1,37 +1,46 @@
 package ntu.scse.cz2002.restaurant.dataAccess;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DataAccessor {
+	
+	public static final Path DATAPATH = Paths.get(System.getProperty("user.dir"), 
+                                        "src/ntu/scse/cz2002/restaurant/dataAccess");
+
+	
 	public static int write(String filename, Object data) {
+		
+		Path fileDataPath = Paths.get(DATAPATH.toString(), filename);
 
 		try {
-        FileOutputStream f = new FileOutputStream(filename);
+			FileOutputStream f = new FileOutputStream(fileDataPath.toString());
 			ObjectOutputStream o = new ObjectOutputStream(f);
 
 			o.writeObject(data);
 
 			o.close();
 			f.close();
-      return 0;
+			
+			return 0;
 		} catch (IOException e) {
 			System.out.println("Failed to save data: IOException");
-      return 1;
-    }
+			return 1;
+	    }
 	}
 
 	public static Object read(String filename) {
+		Path fileDataPath = Paths.get(DATAPATH.toString(), filename);
+		
 		Object data = null;
 
 		try {
-        FileInputStream f = new FileInputStream(filename);
+			FileInputStream f = new FileInputStream(fileDataPath.toString());
 			ObjectInputStream o = new ObjectInputStream(f);
 
 			data = o.readObject();
@@ -46,4 +55,5 @@ public class DataAccessor {
 
 		return data;
 	}
+	
 }
